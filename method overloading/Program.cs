88ef90c58493ef633e31fdba9508076dbcef5334
sajib hasan <sajibhasan800080@gmail.com>
2024-10-28 21:24:57 +0600

    written the code of method overloading in 'method overloading' folder

--- method overloading/Program.cs
+++ method overloading/Program.cs
@@ -1,7 +1,25 @@
 ﻿using System;
 
 class Program{
+
+    static int Addition(int x, int y){
+        return x + y;
+    }
+    static double Addition(double x, double y){
+        return x + y;
+    }
+    static string Addition(string x, string y){
+        return x + " " + y;
+    }
     static void Main(string[] args){
-        Console.WriteLine("Hello World");
+        
+        int intAddition = Addition(10, 3);
+        Console.WriteLine("int: " + intAddition);
+
+        double doubleAddition = Addition(30.44, 32.33);
+        Console.WriteLine("double: " + doubleAddition);
+
+        string stringAddition = Addition("Hello", "World");
+        Console.WriteLine("string: " + stringAddition);
     }
 }
--- method overloading/obj/Debug/net8.0/method overloading.AssemblyInfo.cs
+++ method overloading/obj/Debug/net8.0/method overloading.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("method overloading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2606903843c925a8a0c2ed57ead2957511f3248")]
 [assembly: System.Reflection.AssemblyProductAttribute("method overloading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("method overloading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
